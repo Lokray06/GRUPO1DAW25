3d43bf585a0ff577de889700efad2ecac40996ef
Lokray06 <jpgp14s06@gmail.com>
2025-11-28 10:50:52 +0100

    Commit inicial

---
+++ app/Config.php
@@ -0,0 +1,11 @@
+<?php
+
+ class Config
+ {
+     static public $mvc_bd_hostname = "localhost";
+     static public $mvc_bd_nombre   = "alimentos2025";
+     static public $mvc_bd_usuario  = "danielu";
+     static public $mvc_bd_clave    = "Q1GuQDIX";
+     static public $mvc_vis_css     = "estilo.css";
+ }
+?>
